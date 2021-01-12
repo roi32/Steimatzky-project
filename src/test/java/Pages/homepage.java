20ccddd5a8fd1001a6baec1b0d0d042293d1d9d6
roi32 <75878108+roi32@users.noreply.github.com>
2021-01-12 15:43:51 +0200

    homepage

---
+++ src/test/java/Pages/homepage.java
@@ -0,0 +1,19 @@
+package Pages;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class homepage {
+  @Test
+  public void f() {
+  }
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
